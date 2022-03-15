099f37bf0173f2969a4fe097b43d97c0c158cd39
diogeneshas <diogenes.henrique.br@gmail.com>
2022-03-15 08:12:21 -0300

    Implement tickets display

--- spec/features/creating_tickets_spec.rb
+++ spec/features/creating_tickets_spec.rb
@@ -13,13 +13,13 @@ RSpec.feature "Users can create new tickets" do
     fill_in "Description", with: "My pages are ugly!"
     click_button "Create Ticket"
 
-    expect(page).to have_content "Ticket has been created."
+    expect(page).to have_content "Ticket has been created"
   end
 
   scenario "when providing invalid attributes" do
     click_button "Create Ticket"
 
-    expect(page).to have_content "Ticket has not been created."
+    expect(page).to have_content "Ticket has not been created"
     expect(page).to have_content "Name can't be blank"
     expect(page).to have_content "Description can't be blank"
   end
