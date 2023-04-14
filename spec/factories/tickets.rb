e33d35591c8d1b310622b4902d8fc8a5605f2b37
diogeneshas <diogenes.henrique.br@gmail.com>
2023-04-14 07:47:33 -0300

    Tickets can now be edited

--- spec/factories/tickets.rb
+++ spec/factories/tickets.rb
@@ -1,7 +1,10 @@
 FactoryBot.define do
   factory :ticket do
     name { "MyString" }
-    description { "MyText" }
+    description do
+      "Rails, yet again, works out from the
+    Array what route you wish to follow."
+    end
     project { nil }
   end
 end
